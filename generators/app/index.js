976520668dbb2c54a24686245db623e554c40f0a
Fred Collman <fredcollman@gmail.com>
2019-01-06 12:03:10 +0000

    Generate package.json

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,39 +4,35 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the superior ${chalk.red(
-          "generator-node-minimal"
-        )} generator!`
-      )
-    );
+  async prompting() {
+    this.log("Let's set up a new Node.js package!");
 
     const prompts = [
       {
-        type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
+        type: "input",
+        name: "name",
+        message: "Your package name",
+        default: this.appname.replace(/\s+/g, "-")
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Describe your package",
+        default: ""
       }
     ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.props = await this.prompt(prompts);
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
+    this.fs.copyTpl(
+      this.templatePath("package.json"),
+      this.destinationPath("package.json"),
+      this.props
     );
   }
 
-  install() {
-    this.installDependencies();
-  }
+  // install() {
+  //   this.installDependencies();
+  // }
 };
